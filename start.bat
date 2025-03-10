745576ba5bf252f265b982b6c4bbcf8dcbd4abba
nordichat <alexander.salipur@gmail.com>
2025-03-11 00:54:51 +0100

    Refactor Stalker character configurations
    - Removed outdated `config.txt`
    - Added individual configuration files for Stalker characters (`config_1.txt` to `config_8.txt`)
    - Created shared configuration file `config_shared.txt`
    - Added placeholder for skill configurations in `skill.txt`
    - Updated `start.bat` to launch multiple Stalker configurations
    - Added changelog entry for Stalker configurations

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+start "" wxstart.exe --config=control\config\stalker\config_1.txt
+start "" wxstart.exe --config=control\config\stalker\config_2.txt
+start "" wxstart.exe --config=control\config\stalker\config_3.txt
+start "" wxstart.exe --config=control\config\stalker\config_4.txt
+start "" wxstart.exe --config=control\config\stalker\config_5.txt
+start "" wxstart.exe --config=control\config\stalker\config_6.txt
+start "" wxstart.exe --config=control\config\stalker\config_7.txt
+start "" wxstart.exe --config=control\config\stalker\config_8.txt
