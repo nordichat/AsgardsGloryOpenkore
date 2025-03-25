7bee65cb4c6d076866b32d0cc6d3f3c63792f4f3
nordichat <alexander.salipur@gmail.com>
2025-03-25 15:11:10 +0100

    Fix typos in configuration file paths, update monster priorities, adjust item handling, and enhance roadmap and statistics tracking.

--- start without main.bat
+++ start without main.bat
@@ -1,14 +1,14 @@
 @echo off
-start "" start.exe --config=control\config\stalker\config_2.txt --storage=log\storage_2.txt
+start "" wxstart.exe --config=control\config\stalker\config_2.txt --storage=log\storage_2.txt
 timeout /t 5 /nobreak >nul
-start "" start.exe --config=control\config\stalker\config_3.txt --storage=log\storage_3.txt
+start "" wxstart.exe --config=control\config\stalker\config_3.txt --storage=log\storage_3.txt
 timeout /t 5 /nobreak >nul
-start "" start.exe --config=control\config\stalker\config_4.txt --storage=log\storage_4.txt
+start "" wxstart.exe --config=control\config\stalker\config_4.txt --storage=log\storage_4.txt
 timeout /t 5 /nobreak >nul
-start "" start.exe --config=control\config\stalker\config_5.txt --storage=log\storage_5.txt
+start "" wxstart.exe --config=control\config\stalker\config_5.txt --storage=log\storage_5.txt
 timeout /t 5 /nobreak >nul
-start "" start.exe --config=control\config\stalker\config_6.txt --storage=log\storage_6.txt
+start "" wxstart.exe --config=control\config\stalker\config_6.txt --storage=log\storage_6.txt
 timeout /t 5 /nobreak >nul
-start "" start.exe --config=control\config\stalker\config_7.txt --storage=log\storage_7.txt
+start "" wxstart.exe --config=control\config\stalker\config_7.txt --storage=log\storage_7.txt
 timeout /t 5 /nobreak >nul
-start "" start.exe --config=control\config\stalker\config_8.txt --storage=log\storage_8.txt
+start "" wxstart.exe --config=control\config\stalker\config_8.txt --storage=log\storage_8.txt
