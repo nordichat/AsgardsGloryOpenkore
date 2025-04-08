9e14ba3773614141800b2e86bdcd9103715f31a0
nordichat <alexander.salipur@gmail.com>
2025-04-08 05:49:23 +0200

    Configuration Updates for Shrine of Gonryun Queen (gon_dun01) (#5)
    * Update stream_online_style.css to document char_name_order=none parameter
    
    * Rouge config: Shrine of Gonryun Queen (gon_dun01)
    
    * Final edits before PR.
    
    * Final edits before PR.

--- start.bat
+++ start.bat
@@ -1,9 +1,16 @@
 @echo off
-start "" wxstart.exe --config=control\config\stalker\config_1.txt
-start "" wxstart.exe --config=control\config\stalker\config_2.txt
-start "" wxstart.exe --config=control\config\stalker\config_3.txt
-start "" wxstart.exe --config=control\config\stalker\config_4.txt
-start "" wxstart.exe --config=control\config\stalker\config_5.txt
-start "" wxstart.exe --config=control\config\stalker\config_6.txt
-start "" wxstart.exe --config=control\config\stalker\config_7.txt
-start "" wxstart.exe --config=control\config\stalker\config_8.txt
+start "" wxstart.exe --config=control\config\stalker\config_1.txt --console-log=logs\console_1.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_2.txt --storage-log=logs\storage_2.txt --console-log=logs\console_2.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_3.txt --storage-log=logs\storage_3.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_4.txt --storage-log=logs\storage_4.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_5.txt --storage-log=logs\storage_5.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_6.txt --storage-log=logs\storage_6.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_7.txt --storage-log=logs\storage_7.txt
+timeout /t 3 /nobreak >nul
+start "" wxstart.exe --config=control\config\stalker\config_8.txt --storage-log=logs\storage_8.txt
